e8627e6d50fcd811912bd352825990d12f1cc058
pongo1231 <pongo1999712@gmail.com>
2025-01-02 00:42:48 +0000

    bot_fortress: apply heavy minigun offset for all primary weapons

--- utils/RCBot2_meta/bot_fortress.cpp
+++ utils/RCBot2_meta/bot_fortress.cpp
@@ -7054,7 +7054,7 @@ void CBotTF2 ::modAim(edict_t *pEntity, Vector &v_origin, Vector *v_desired_offs
 
 		if (m_iClass == TF_CLASS_HWGUY)
 		{
-			if (pWp->getID() == TF2_WEAPON_MINIGUN)
+			if (pWepInfo->getSlot() == 0)
 			{
 				Vector vForward;
 				Vector vRight;
